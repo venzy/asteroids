5ed51b5b15b8e40d1b8cf9fe037a45b406cac554
venzy <david.venz@gmail.com>
2024-10-19 09:26:51 +1000

    Use groups to manage sprites

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	# New Player sprites will automatically be added to these containers upon construction
+	Player.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	# Main loop
@@ -21,11 +27,13 @@ def main():
 				return
 		
 		# Update game objects
-		player.update(dt)
+		for sprite in updatable:
+			sprite.update(dt)
 		
 		# Paint screen
 		screen.fill(pygame.Color(0, 0, 0))
-		player.draw(screen)
+		for sprite in drawable:
+			sprite.draw(screen)
 		pygame.display.flip()
 
 		dt = clock.tick(FRAME_RATE) / 1000
