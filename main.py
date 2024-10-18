87d1b0bb742aca4c5198de3ef1a02808d8d17594
venzy <david.venz@gmail.com>
2024-10-19 08:40:01 +1000

    Black screen and quit handling

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from constants import *
 
 def main():
 	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	# Main loop
+	while True:
+		# Handle quit
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		
+		# Paint screen
+		screen.fill(pygame.Color(0, 0, 0))
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
